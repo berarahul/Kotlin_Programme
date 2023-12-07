ccf2b43f17deb1270d71762695da366e9403f165
Rahul <rb6764385@gmail.com>
2023-12-07 10:12:59 +0530

    kotlin

---
+++ src/main/kotlin/number.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>){
+
+    val a=1234
+    println(a::class.java)
+
+
+}
---
+++ src/main/kotlin/stringTemplate.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>){
+
+
+    val a= "Arpan"
+    val b=12
+    val c=13
+    //var d= b+c
+    println("My name is $a")
+    println("Sum of Two number is ${b+c}")
+    //is called String Template
+
+}
